86bc19eca0b8028eaf3a60333dfa4af3060aa33f
Irving Wang <wang5952@purdue.edu>
2025-03-16 12:39:17 -0400

    build save

--- control-base/typec-board-base/cmake/gcc-arm-none-eabi.cmake
+++ control-base/typec-board-base/cmake/gcc-arm-none-eabi.cmake
@@ -1,21 +1,24 @@
 set(CMAKE_SYSTEM_NAME               Generic)
 set(CMAKE_SYSTEM_PROCESSOR          arm)
 
+# Set ARM toolchain path (update this path to match your installation)
+set(ARM_TOOLCHAIN_PATH "/Applications/ArmGNUToolchain/10.2/bin")
+
 set(CMAKE_C_COMPILER_FORCED TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
-set(CMAKE_C_COMPILER_ID GNU)
-set(CMAKE_CXX_COMPILER_ID GNU)
+set(CMAKE_ASM_COMPILER_FORCED TRUE)
 
 # Some default GCC settings
-# arm-none-eabi- must be part of path environment
-set(TOOLCHAIN_PREFIX                arm-none-eabi-)
-
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
+# Use absolute paths for the toolchain
+set(TOOLCHAIN_PREFIX                "arm-none-eabi-")
+set(CMAKE_C_COMPILER                "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}gcc")
+set(CMAKE_ASM_COMPILER              "${CMAKE_C_COMPILER}")
+set(CMAKE_CXX_COMPILER              "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}g++")
+set(CMAKE_LINKER                    "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}g++")
+set(CMAKE_OBJCOPY                   "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}objcopy")
+set(CMAKE_SIZE                      "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}size")
+set(CMAKE_AR                        "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}ar")
+set(CMAKE_RANLIB                    "${ARM_TOOLCHAIN_PATH}/${TOOLCHAIN_PREFIX}ranlib")
 
 set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
@@ -38,9 +41,10 @@ endif()
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
+# Fix the linking issue with _exit symbol
 set(CMAKE_C_LINK_FLAGS "${TARGET_FLAGS}")
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -T \"${CMAKE_SOURCE_DIR}/STM32F407IGHx_FLASH.ld\"")
-set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} --specs=nano.specs")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} --specs=nano.specs --specs=nosys.specs")
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lc -lm -Wl,--end-group")
 set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
