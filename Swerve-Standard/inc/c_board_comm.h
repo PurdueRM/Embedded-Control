f16c9c5bec20eaa04db7a4f2c26df42aa12e63bd
Polarbearbo <luffysir2@gmail.com>
2025-06-16 19:33:36 -0400

    add debugging for c board communication

--- Swerve-Standard/inc/c_board_comm.h
+++ Swerve-Standard/inc/c_board_comm.h
@@ -4,7 +4,7 @@
 
 // Define/undefine this as needed when flashing 
 // the two c boards on the swerve robot
-#define MASTER
+// #define MASTER
 #define C_BOARD_COMM_PERIOD (10) // ms
 
 #include <stdint.h>
