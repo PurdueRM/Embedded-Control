57fc4feede57837b83970e29b3fbd3019ece0a3c
Polarbearbo <luffysir2@gmail.com>
2025-06-16 19:54:44 -0400

    fix logic of can comm

--- Swerve-Standard/src/c_board_comm.c
+++ Swerve-Standard/src/c_board_comm.c
@@ -6,8 +6,8 @@
 #include "referee_system.h"
 #include "supercap.h"
 Board_Comm_Package_t g_board_comm_package; // 
-CAN_Instance_t *g_board_master; // the master board 
-CAN_Instance_t *g_board_slave; // the slave board
+CAN_Instance_t *g_board_communication; // the master board 
+// CAN_Instance_t *g_board_slave; // the slave board
 extern Referee_System_t Referee_System;
 extern Supercap_t g_supercap;
 uint8_t g_board_comm_package_first_part_established = 0; // flag to check if the first part of the package is established
@@ -22,19 +22,19 @@ uint8_t slave_debug = 0;
 void C_Board_Comm_Task_Init() {
     #ifdef MASTER 
         #pragma message "Master C_Board_Comm_Task_Init() is complied"
-        g_board_master = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
-        g_board_slave = CAN_Device_Register(1, 0x301, 0x311, C_Board_Recv_Ref_Info);
+        // g_board_master = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
+        // g_board_slave = CAN_Device_Register(1, 0x301, 0x311, C_Board_Recv_Ref_Info);
 
         master_debug = 1;
 
-        //g_board_master = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
+        g_board_communication = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
     #else 
         #pragma message "Slave C_Board_Comm_Task_Init() is compiled"
-        g_board_slave = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Supercap_Info);
-        g_board_master = CAN_Device_Register(1, 0x311, 0x301, C_Board_Recv_Ref_Info);
+        // g_board_slave = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Supercap_Info);
+        // g_board_master = CAN_Device_Register(1, 0x311, 0x301, C_Board_Recv_Ref_Info);
 
         slave_debug = 1;
-        //g_board_slave = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Ref_Info);
+        g_board_communication = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Ref_Info);
     #endif
 }
 
@@ -67,9 +67,9 @@ void C_Board_Comm_Send_Loop()
 {
     #ifdef MASTER
         float temp_power_limit = 69.0;
-        memcpy(&(g_board_master->tx_buffer[0]), &(temp_power_limit), sizeof(float)); // &(Referee_System.Robot_State.Chassis_Power_Max)
+        memcpy(&(g_board_communication->tx_buffer[0]), &(temp_power_limit), sizeof(float)); // &(Referee_System.Robot_State.Chassis_Power_Max)
     #else
-        memcpy(&(g_board_slave->tx_buffer[0]), &(g_supercap.Vo), sizeof(float)); // Vo
-        memcpy(&(g_board_slave->tx_buffer[sizeof(float)]), &(g_supercap.Ps), sizeof(float)); // Ps
+        memcpy(&(g_board_communication->tx_buffer[0]), &(g_supercap.Vo), sizeof(float)); // Vo
+        memcpy(&(g_board_communication->tx_buffer[sizeof(float)]), &(g_supercap.Ps), sizeof(float)); // Ps
     #endif
 }
