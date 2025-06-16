cdaac2e6fd8f5253e3cc8c89376a650907af5f63
andyNiu <aniu2240@gmail.com>
2025-06-16 19:02:45 -0400

    Fix init function?

--- Swerve-Standard/src/c_board_comm.c
+++ Swerve-Standard/src/c_board_comm.c
@@ -15,15 +15,17 @@ extern Supercap_t g_supercap;
  * @brief
  * @param
  */
-void C_Board_Comm_Task_init() {
+void C_Board_Comm_Task_Init() {
     #ifdef MASTER 
         #pragma message "Master C_Board_Comm_Task_Init() is complied"
+        // g_board_master = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
+        // g_board_slave = CAN_Device_Register(1, 0x301, 0x311, C_Board_Recv_Ref_Info);
         g_board_master = CAN_Device_Register(1, 0x310, 0x300, C_Board_Recv_Supercap_Info);
-        g_board_slave = CAN_Device_Register(1, 0x301, 0x311, C_Board_Recv_Ref_Info);
     #else 
         #pragma message "Slave C_Board_Comm_Task_Init() is compiled"
         // g_board_master = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Supercap_Info);
         // g_board_slave = CAN_Device_Register(1, 0x311, 0x301, C_Board_Recv_Ref_Info);
+        g_board_slave = CAN_Device_Register(1, 0x300, 0x310, C_Board_Recv_Ref_Info);
     #endif
 }
 
@@ -54,7 +56,7 @@ void C_Board_Comm_Send_Loop()
 {
     #ifdef MASTER
         float temp_power_limit = 69.0;
-        memcpy(&(g_board_master->tx_buffer), &(temp_power_limit), sizeof(float)); // &(Referee_System.Robot_State.Chassis_Power_Max)
+        memcpy(&(g_board_master->tx_buffer[0]), &(temp_power_limit), sizeof(float)); // &(Referee_System.Robot_State.Chassis_Power_Max)
     #else
         memcpy(&(g_board_slave->tx_buffer[0]), &(g_supercap.Vo), sizeof(float)); // Vo
         memcpy(&(g_board_slave->tx_buffer[sizeof(float)]), &(g_supercap.Ps), sizeof(float)); // Ps
