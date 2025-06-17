d5c9c26e1df1426a646e8af2568af4b2d19f6108
Polarbearbo <luffysir2@gmail.com>
2025-06-17 18:51:53 -0400

    init swerve standard slave board

---
+++ Swerve-Standard-Slave/inc/c_board_comm.h
@@ -0,0 +1,28 @@
+// Referenced https://github.com/PurdueRM/wlb/blob/main/src/app/inc/board_comm_task.h
+#ifndef __C_BOARD_COMM_H
+#define __C_BOARD_COMM_H
+
+// Define/undefine this as needed when flashing 
+// the two c boards on the swerve robot
+// #define MASTER
+#define C_BOARD_COMM_PERIOD (100) // ms
+
+#include <stdint.h>
+#include "bsp_uart.h"
+
+typedef struct Board_Comm_Package_s 
+{
+    // from slave to master
+    float Vo; // the voltage of the super cap
+    float Ps; // power reference from the super cap
+
+    // from master to slave
+    uint16_t power_limit; //send power from the master to the slave
+} Board_Comm_Package_t;
+
+void C_Board_Comm_Task_Init();
+void C_Board_Recv_Ref_Info();
+void C_Board_Recv_Supercap_Info(); // send 
+void C_Board_Comm_Send_Loop();
+
+#endif
---
+++ Swerve-Standard-Slave/src/motor_task.c
@@ -0,0 +1,27 @@
+#include "motor_task.h"
+#include "dji_motor.h"
+// #include "dm_motor.h"
+// #include "mf_motor.h"
+#include "supercap.h"
+#include "c_board_comm.h"
+extern Supercap_t g_supercap;
+extern CAN_Instance_t* g_board_communication;
+// extern CAN_Instance_t* g_board_master;
+extern uint8_t g_board_comm_sending_pending;
+uint8_t result_can = 0;
+void Motor_Task_Loop() {
+    #ifdef MASTER
+        DJI_Motor_Send();
+    #else
+        if (g_board_comm_sending_pending == 1)
+        {
+            Supercap_Send();
+        }
+    #endif
+    if (g_board_comm_sending_pending == 1)
+    {
+        result_can = CAN_Transmit(g_board_communication);
+        g_board_comm_sending_pending = 0;
+    }
+}
+
