58a96a4eb2acd0ae1faad9a36aa055e96d6161cd
andyNiu <aniu2240@gmail.com>
2025-06-16 21:45:33 -0400

    Added a send flag to command CAN_Transmit at the appropriate time

--- Swerve-Standard/src/c_board_comm.c
+++ Swerve-Standard/src/c_board_comm.c
@@ -14,6 +14,9 @@ uint8_t g_board_comm_package_first_part_established = 0; // flag to check if the
 uint8_t g_board_comm_package_second_part_established = 0; // flag to check if the second part of the package is established
 uint8_t master_debug = 0;
 uint8_t slave_debug = 0;
+float entered_send = 0.0;
+
+uint8_t g_board_comm_sending_pending = 0;
 
 /**
  * @brief
@@ -68,8 +71,12 @@ void C_Board_Comm_Send_Loop()
     #ifdef MASTER
         float temp_power_limit = 69.0;
         memcpy(&(g_board_communication->tx_buffer[0]), &(temp_power_limit), sizeof(float)); // &(Referee_System.Robot_State.Chassis_Power_Max)
+        g_board_comm_sending_pending = 1;
     #else
         memcpy(&(g_board_communication->tx_buffer[0]), &(g_supercap.Vo), sizeof(float)); // Vo
         memcpy(&(g_board_communication->tx_buffer[sizeof(float)]), &(g_supercap.Ps), sizeof(float)); // Ps
+        g_board_comm_sending_pending = 1;
     #endif
+
+    entered_send += 0.0001; // TODO DELETE LATER AFTER DEBUGGING. CAUSES MEMORY ERROR
 }
