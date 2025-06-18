df37b75fdad79a58773a59215abfe10df4ccd88a
jia-xie <103150184+jia-xie@users.noreply.github.com>
2025-06-18 11:00:12 -0400

    added referee system

--- Swerve-Standard/src/robot.c
+++ Swerve-Standard/src/robot.c
@@ -66,7 +66,7 @@ void Handle_Starting_Up_State()
         Gimbal_Task_Init();
         Launch_Task_Init();
         Jetson_Orin_Init(&huart6);
-
+        Referee_System_Init(&huart1);
         Remote_Init(&huart3);  
     #else
         // Initialize the slave c board for the supercaps
@@ -170,14 +170,20 @@ void Process_Remote_Input()
         g_robot_state.chassis.IS_SPINTOP_ENABLED ^= 0x01;
     }
 
+    if (g_remote.controller.left_switch == MID) {
+        g_robot_state.chassis.IS_SPINTOP_ENABLED = 1;
+    } else {
+        g_robot_state.chassis.IS_SPINTOP_ENABLED = 0;
+    }
+
     if (g_remote.controller.left_switch == UP) { // Left switch high to enable spintop
         //g_robot_state.chassis.IS_SPINTOP_ENABLED = 1;
-        //g_robtot_state.launch.IS_FIRING_ENABLED = 1;
-        g_robot_state.launch.IS_AUTO_AIMING_ENABLED = 1;
+        g_robot_state.launch.IS_FIRING_ENABLED = 1;
+        // g_robot_state.launch.IS_AUTO_AIMING_ENABLED = 1;
     } else {
         //g_robot_state.chassis.IS_SPINTOP_ENABLED = 0;
-        //g_robot_state.launch.IS_FIRING_ENABLED = 0;
-        g_robot_state.launch.IS_AUTO_AIMING_ENABLED = 0;
+        g_robot_state.launch.IS_FIRING_ENABLED = 0;
+        // g_robot_state.launch.IS_AUTO_AIMING_ENABLED = 0;
     }
 
     // Update previous states keyboard
