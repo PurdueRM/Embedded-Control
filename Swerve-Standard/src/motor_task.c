a75306ba1d2097434c5d20b79a580e5660f8fa76
jia-xie <103150184+jia-xie@users.noreply.github.com>
2025-06-20 23:21:32 -0400

    add can daemon

--- Swerve-Standard/src/motor_task.c
+++ Swerve-Standard/src/motor_task.c
@@ -4,12 +4,29 @@
 // #include "mf_motor.h"
 #include "supercap.h"
 #include "c_board_comm.h"
+#include "bsp_daemon.h"
+#include "bsp_can.h"
 extern Supercap_t g_supercap;
 extern CAN_Instance_t* g_board_communication;
 // extern CAN_Instance_t* g_board_master;
 extern uint8_t g_board_comm_sending_pending;
 uint8_t result_can = 0;
+Daemon_Instance_t *g_motor_task_daemon_ptr = NULL;
+#define MOTOR_TIMEOUT_MS (100) // 1 second timeout for motor task
+
+void Motor_Task_Timeout_Callback(void) {
+    CAN_Service_Restart();
+}
+
+void Motor_Task_Init() {
+    uint16_t reload_value = MOTOR_TIMEOUT_MS / DAEMON_PERIOD;
+	uint16_t initial_counter = reload_value;
+	g_motor_task_daemon_ptr = Daemon_Register(reload_value, initial_counter, Motor_Task_Timeout_Callback);
+}
+
+
 void Motor_Task_Loop() {
+    Daemon_Reload(g_motor_task_daemon_ptr);
     #ifdef MASTER
         DJI_Motor_Send();
     #else
