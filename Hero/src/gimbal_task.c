91523353f1afdd3899a18e1770463f37990e83d2
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-06-24 21:10:16 -0700

    disabled gimbal

--- Hero/src/gimbal_task.c
+++ Hero/src/gimbal_task.c
@@ -29,7 +29,7 @@ void Gimbal_Task_Init()
             {
                 .kp = 300.0f,
                 .kd = 30.0f,
-                .output_limit = GM6020_MAX_CURRENT,
+                .output_limit = GM6020_MAX_VOLTAGE_INT,
             },
         .velocity_pid =
             {
@@ -38,7 +38,7 @@ void Gimbal_Task_Init()
                 .kf = 100.0f,
                 .feedforward_limit = 5000.0f,
                 .integral_limit = 1000.0f,
-                .output_limit = GM6020_MAX_CURRENT,
+                .output_limit = GM6020_MAX_VOLTAGE_INT,
             },
     };
 
@@ -59,20 +59,20 @@ void Gimbal_Task_Init()
 
 void Gimbal_Ctrl_Loop()
 {
-    g_robot_state.gimbal.yaw_angle -= g_remote.controller.right_stick.x/660.0f * 0.01f + g_remote.mouse.x / 10000.0f;
-    g_robot_state.gimbal.pitch_angle += g_remote.controller.right_stick.y / 660.0f * 0.01f + g_remote.mouse.y / 50000.0f;
+    // g_robot_state.gimbal.yaw_angle -= g_remote.controller.right_stick.x/660.0f * 0.01f + g_remote.mouse.x / 10000.0f;
+    // g_robot_state.gimbal.pitch_angle += g_remote.controller.right_stick.y / 660.0f * 0.01f + g_remote.mouse.y / 50000.0f;
 
-    // hardware limits for gimbal pitch (prevent self collision)
-    __MAP_ANGLE_TO_UNIT_CIRCLE(g_robot_state.gimbal.yaw_angle);
-    // __MAX_LIMIT(g_robot_state.gimbal.pitch_angle, -0.4f, 0.4f);
+    // // hardware limits for gimbal pitch (prevent self collision)
+    // __MAP_ANGLE_TO_UNIT_CIRCLE(g_robot_state.gimbal.yaw_angle);
+    // // __MAX_LIMIT(g_robot_state.gimbal.pitch_angle, -0.4f, 0.4f);
 
-    // Control loop for gimbal
-    // DJI_Motor_Set_Angle(g_pitch, g_robot_state.gimbal.pitch_angle);
-    DJI_Motor_Set_Angle(g_yaw, g_robot_state.gimbal.yaw_angle);
-    // __MAX_LIMIT(g_robot_state.gimbal.pitch_angle, -0.45f, 0.4f);
+    // // Control loop for gimbal
+    // // DJI_Motor_Set_Angle(g_pitch, g_robot_state.gimbal.pitch_angle);
+    // DJI_Motor_Set_Angle(g_yaw, g_robot_state.gimbal.yaw_angle);
+    // // __MAX_LIMIT(g_robot_state.gimbal.pitch_angle, -0.45f, 0.4f);
     
-    // DM_Motor_Enable_Motor(g_pitch);
-    // DM_Motor_Ctrl_MIT_PD(g_pitch, g_robot_state.gimbal.pitch_angle, 0.0f, 0.0f, 20.0f, 8.5f);
+    // // DM_Motor_Enable_Motor(g_pitch);
+    // // DM_Motor_Ctrl_MIT_PD(g_pitch, g_robot_state.gimbal.pitch_angle, 0.0f, 0.0f, 20.0f, 8.5f);
 }
 
 void _Gimbal_Target_Reset()
