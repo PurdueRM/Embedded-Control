c1e02c0bc44a9181e7ccba04b41a8eef98e681ba
Yi <0418yicheng@gmail.com>
2026-06-29 23:26:19 -0400

    Basically all working. yaw PID tweakign

--- Standard/src/chassis_task.c
+++ Standard/src/chassis_task.c
@@ -4,6 +4,7 @@
 #include "robot.h"
 #include "remote.h"
 #include "dji_motor.h"
+#include "dm_motor.h"
 #include "omni_locomotion.h"
 #include "rate_limiter.h"
 #include "pid.h"
@@ -12,6 +13,9 @@
 #include <math.h>
 #include "c_board_comm.h" 
 
+extern Robot_State_t g_robot_state;
+extern Remote_t g_remote;
+float gimbal_angle_difference;
 pose_2d_t sentry_pose;
 DJI_Motor_Handle_t *motors[4];
 uint8_t drive_esc_id_array[4] = {1, 2, 3, 4};
@@ -22,7 +26,24 @@ Motor_Reversal_t drive_motor_reversal_array[4] = {
     MOTOR_REVERSAL_NORMAL
 };
 
-extern DJI_Motor_Handle_t *g_yaw; // for reading gimbal angle
+extern DM_Motor_Handle_t *g_yaw; // for reading gimbal angle
+
+omni_physical_constants_t physical_constants;
+omni_chassis_state_t chassis_state;
+
+extern Board_Comm_Package_t g_board_comm_package;
+
+rate_limiter_t wheel_rate_limiters[4];
+PID_t g_follow_gimbal_angle_pid;                      // chassis following gimbal
+
+pose_2d_t sentry_pose;
+motor_data_t motor_data_odom;
+
+uint16_t last_hp;
+uint16_t is_hit_counter = 0;
+
+float omega = 1;
+int omega_bool = 0;
 
 void Chassis_Task_Init(){
     // Init chassis hardware
@@ -46,163 +67,166 @@ void Chassis_Task_Init(){
 
     sentry_pose.x = 0;
     sentry_pose.y = 0;
+
+    pose_2d_t init_pose = {
+        .x = INIT_X_POS,
+        .y = INIT_Y_POS,
+        .theta = INIT_THETA
+    };
+
+    physical_constants = omni_init( //TODO: Set the actual constants for standard
+        CHASSIS_WHEEL_DIAMETER,
+        CHASSIS_RADIUS,
+        CHASSIS_MOUNTING_ANGLE,
+        CHASSIS_MAX_SPEED,
+        &init_pose
+    );
+
+    chassis_state.v_x = 0.0f;
+    chassis_state.v_y = 0.0f;
+    chassis_state.omega = 0.0f;
+
+    for (int i = 0; i < 4; i++) {
+        // configure rate limiters
+        rate_limiter_init(&wheel_rate_limiters[i], MAX_ABC);
+    }
+
+    // Init PID
+    PID_Init(&g_follow_gimbal_angle_pid, 10, 0, 2500, 2*PI*30, 0, 0);
+    sentry_pose.x = 0;
+    sentry_pose.y = 0;
+
+    last_hp = Referee_System.Robot_State.Remaining_HP;
 }
 
-void Chassis_Ctrl_Loop(){
-    DJI_Motor_Set_Velocity(motors[0], 100);
-    DJI_Motor_Set_Velocity(motors[1], 100);
-    DJI_Motor_Set_Velocity(motors[2], 100);
-    DJI_Motor_Set_Velocity(motors[3], 100);
-}
+void Chassis_Process_Target_Velocity()
+{    
+    chassis_state.v_x_in_gimbal = g_remote.controller.left_stick.x / 660.0f * 4.0f;
+    chassis_state.v_y_in_gimbal = g_remote.controller.left_stick.y / 660.0f * 4.0f;
+    // chassis_state.omega_in_gimbal = g_remote.controller.wheel * MAX_ANGLUAR_SPEED;
 
-// extern Board_Comm_Package_t g_board_comm_package; // 
-// extern Robot_State_t g_robot_state;
-// extern Remote_t g_remote;
-// float gimbal_angle_difference;
+    gimbal_angle_difference =  g_yaw->stats->pos;
 
+    // static float time_for_omega = 0.0f;
+    float chassis_omega_new_target = 0;
+    const float hit_timeout = 5; // seconds
 
+    // If the robot is hit, increase spintop rate
+    if(Referee_System.Robot_State.Remaining_HP < last_hp) {
+        is_hit_counter = 500 * hit_timeout;
+    }
 
-// omni_physical_constants_t physical_constants;
-// omni_chassis_state_t chassis_state;
-
-// rate_limiter_t wheel_rate_limiters[4];
-// PID_t g_follow_gimbal_angle_pid;                      // chassis following gimbal
-
-// pose_2d_t sentry_pose;
-// motor_data_t motor_data_odom;
-
-// uint16_t last_hp;
-// uint16_t is_hit_counter = 0;
-
-
-
-// void Chassis_Task_Init()
-// {
-//     
-
-//     pose_2d_t init_pose = {
-//         .x = INIT_X_POS,
-//         .y = INIT_Y_POS,
-//         .theta = INIT_THETA
-//     };
-
-//     // Initialize the omni chassis state
-//     physical_constants = omni_init(
-//         CHASSIS_WHEEL_DIAMETER,
-//         CHASSIS_RADIUS,
-//         CHASSIS_MOUNTING_ANGLE,
-//         CHASSIS_MAX_SPEED,
-//         &init_pose
-//     );
-
-//     chassis_state.v_x = 0.0f;
-//     chassis_state.v_y = 0.0f;
-//     chassis_state.omega = 0.0f;
-
-//     for (int i = 0; i < 4; i++) {
-//         // configure rate limiters
-//         rate_limiter_init(&wheel_rate_limiters[i], MAX_ABC);
-//     }
-
-//     // Init PID
-//     PID_Init(&g_follow_gimbal_angle_pid, 20, 0, 2500, 2*PI*30, 0, 0);
-//     sentry_pose.x = 0;
-//     sentry_pose.y = 0;
-
-//     last_hp = Referee_System.Robot_State.Remaining_HP;
-// }
-
-// void Chassis_Process_Target_Velocity()
-// {
-//     // static float time_for_omega = 0.0f;
-//     // time_for_omega += 0.001f;
-//     if (g_remote.controller.right_switch == UP)
-//     {
-//         chassis_state.v_x_in_gimbal = -g_orin_data.receiving.navigation.y_vel;
-//         chassis_state.v_y_in_gimbal = g_orin_data.receiving.navigation.x_vel;
-//     } else {
-//         chassis_state.v_x_in_gimbal = g_remote.controller.left_stick.x / 660.0f * 4.0f;
-//         chassis_state.v_y_in_gimbal = g_remote.controller.left_stick.y / 660.0f * 4.0f;
-//         chassis_state.omega_in_gimbal = g_remote.controller.wheel * MAX_ANGLUAR_SPEED;
-//     }
-
-//     float chassis_omega_new_target = 0;
-//     const float hit_timeout = 5; // seconds
-
-//     // If the robot is hit, increase spintop rate
-//     if(Referee_System.Robot_State.Remaining_HP < last_hp) {
-//         is_hit_counter = 500 * hit_timeout;
-//     }
-
-//     //TODO：Adjust the data type to reflect the actual value.
-//     last_hp = Referee_System.Robot_State.Remaining_HP;
+    //TODO：Adjust the data type to reflect the actual value.
+    last_hp = Referee_System.Robot_State.Remaining_HP;
     
-//     // Counter for hit timeout
-//     if (is_hit_counter > 0) {
-//         is_hit_counter--;
-//     }
+    // Counter for hit timeout
+    if (is_hit_counter > 0) {
+        is_hit_counter--;
+    }
 
-//     if (g_robot_state.chassis.IS_SPINTOP_ENABLED) {
+    if (g_robot_state.chassis.IS_SPINTOP_ENABLED /*|| g_remote.controller.left_switch == UP*/ ) {
         
-//         if (is_hit_counter > 0 && g_board_comm_package.Vo > 13.5f) {
-//             chassis_omega_new_target = 9.5 * PI; // 8 * PI rad/s
-//         }
-//         else if (is_hit_counter > 0)
-//         {
-//             chassis_omega_new_target = 4.5 * PI;
-//         }
-//         else {  //Decrease spintop rate if not hit for a while
-//             chassis_omega_new_target = 2 * PI; // 2 * PI rad/s
-//         }
-//         __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
+        if (is_hit_counter > 0) {
+            chassis_omega_new_target = 6 * PI; // 8 * PI rad/s
+        }
+        else {  //Decrease spintop rate if not hit for a while
+            chassis_omega_new_target = omega; // 2 * PI rad/s
+        }
+        __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
 
-//         // float frenquncy = 0.1f;
-//         // speed_up_spintop_rate = 8 * PI * sin(2*PI*frenquncy*time_for_omega);
-//         // __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
+        // float frenquncy = 0.1f;
+        // speed_up_spintop_rate = 8 * PI * sin(2*PI*frenquncy*time_for_omega);
+        // __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
        
-//     } else {
-//         // chassis_state.omega = g_robot_state.chassis.omega * MAX_ANGLUAR_SPEED;
-//         __MAP_ANGLE_TO_UNIT_CIRCLE(gimbal_angle_difference);
-//         chassis_omega_new_target = PID(&g_follow_gimbal_angle_pid, gimbal_angle_difference);
-//         __MAX_LIMIT(chassis_omega_new_target, -6*2*PI, 6*2*PI);
-//         // chassis_omega_new_target = -g_remote.controller.right_stick.x/660.0f * 6 * PI;
-//         __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
-//     }
-//     // __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.003f);
+    } else {
+        // chassis_state.omega = g_robot_state.chassis.omega * MAX_ANGLUAR_SPEED;
+        __MAP_ANGLE_TO_UNIT_CIRCLE(gimbal_angle_difference);
+        chassis_omega_new_target = PID(&g_follow_gimbal_angle_pid, gimbal_angle_difference);
+        __MAX_LIMIT(chassis_omega_new_target, -6*2*PI, 6*2*PI);
+        // chassis_omega_new_target = -g_remote.controller.right_stick.x/660.0f * 6 * PI;
+        __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.001f);
+    }
+    // __FIRST_ORDER_FILTER(chassis_state.omega, chassis_omega_new_target, 0.003f);
+    Update_Omega();
+}
 
-// }
+void Chassis_Ctrl_Loop()
+{
+    //TODO: change this, for odom only
+    gimbal_angle_difference = g_yaw->stats->pos; //Convert to abs
+    __MAP_ANGLE_TO_UNIT_CIRCLE(gimbal_angle_difference);
+    Chassis_Process_Target_Velocity();
 
-// void Chassis_Ctrl_Loop()
-// {
-//     //TODO: change this, for odom only
-//     gimbal_angle_difference = DJI_Motor_Get_Absolute_Angle(g_yaw);
-//     Chassis_Process_Target_Velocity();
-    
-//     chassis_state.v_x = chassis_state.v_x_in_gimbal * cos(gimbal_angle_difference) - chassis_state.v_y_in_gimbal * sin(gimbal_angle_difference);
-//     chassis_state.v_y = chassis_state.v_x_in_gimbal * sin(gimbal_angle_difference) + chassis_state.v_y_in_gimbal * cos(gimbal_angle_difference);
-//     // chassis_state.v_x = g_robot_state.input.vx;
-//     // chassis_state.v_y = g_robot_state.input.vy;
+    if (g_robot_state.IS_SUPER_CAPACITOR_ENABLED) {
+        physical_constants.max_speed = 5.0f;
+        __FIRST_ORDER_FILTER(chassis_state.v_x, 2 * (chassis_state.v_x_in_gimbal * cos(gimbal_angle_difference) - chassis_state.v_y_in_gimbal * sin(gimbal_angle_difference)), 0.005f);
+        __FIRST_ORDER_FILTER(chassis_state.v_y, 2 * (chassis_state.v_x_in_gimbal * sin(gimbal_angle_difference) + chassis_state.v_y_in_gimbal * cos(gimbal_angle_difference)), 0.005f)
+    } else {
+        physical_constants.max_speed = 2.0f;
+        chassis_state.v_x = chassis_state.v_x_in_gimbal * cos(gimbal_angle_difference) - chassis_state.v_y_in_gimbal * sin(gimbal_angle_difference);
+        chassis_state.v_y = chassis_state.v_x_in_gimbal * sin(gimbal_angle_difference) + chassis_state.v_y_in_gimbal * cos(gimbal_angle_difference);
+    }
 
-//     // Control loop for the chassis
-//     omni_calculate_kinematics(&chassis_state, &physical_constants);
-//     // omni_desaturate_wheel_speeds(&chassis_state, &physical_constants);
-//     omni_convert_to_rpm(&chassis_state);
+    // Control loop for the chassis
+    omni_calculate_kinematics(&chassis_state, &physical_constants);
+    // omni_desaturate_wheel_speeds(&chassis_state, &physical_constants);2
+    omni_convert_to_rpm(&chassis_state);
 
-//     // use rate limiter to limit acceleration of the wheels
-//     // chassis_state.phi_dot_1 = rate_limiter_iterate(&wheel_rate_limiters[0], chassis_state.phi_dot_1);
-//     // chassis_state.phi_dot_2 = rate_limiter_iterate(&wheel_rate_limiters[1], chassis_state.phi_dot_2);
-//     // chassis_state.phi_dot_3 = rate_limiter_iterate(&wheel_rate_limiters[2], chassis_state.phi_dot_3);
-//     // chassis_state.phi_dot_4 = rate_limiter_iterate(&wheel_rate_limiters[3], chassis_state.phi_dot_4);
+    // use rate limiter to limit acceleration of the wheels
+    // chassis_state.phi_dot_1 = rate_limiter_iterate(&wheel_rate_limiters[0], chassis_state.phi_dot_1);
+    // chassis_state.phi_dot_2 = rate_limiter_iterate(&wheel_rate_limiters[1], chassis_state.phi_dot_2);
+    // chassis_state.phi_dot_3 = rate_limiter_iterate(&wheel_rate_limiters[2], chassis_state.phi_dot_3);
+    // chassis_state.phi_dot_4 = rate_limiter_iterate(&wheel_rate_limiters[3], chassis_state.phi_dot_4);
 
-//     // set the velocities of the wheels
-//     DJI_Motor_Set_Velocity(motors[0], chassis_state.phi_dot_1);
-//     DJI_Motor_Set_Velocity(motors[1], chassis_state.phi_dot_2);
-//     DJI_Motor_Set_Velocity(motors[2], chassis_state.phi_dot_3);
-//     DJI_Motor_Set_Velocity(motors[3], chassis_state.phi_dot_4);
+    // set the velocities of the wheels
+    DJI_Motor_Set_Velocity(motors[0], chassis_state.phi_dot_1);
+    DJI_Motor_Set_Velocity(motors[1], chassis_state.phi_dot_2);
+    DJI_Motor_Set_Velocity(motors[2], chassis_state.phi_dot_3);
+    DJI_Motor_Set_Velocity(motors[3], chassis_state.phi_dot_4);
 
-//     motor_data_odom.front_left = DJI_Motor_Get_Total_Angle(motors[0]) * physical_constants.R;
-//     motor_data_odom.back_left = DJI_Motor_Get_Total_Angle(motors[1]) * physical_constants.R;
-//     motor_data_odom.back_right = DJI_Motor_Get_Total_Angle(motors[2]) * physical_constants.R;
-//     motor_data_odom.front_right = DJI_Motor_Get_Total_Angle(motors[3]) * physical_constants.R;
-//     Update_Omni_Odometry(&sentry_pose, &physical_constants, &motor_data_odom, g_imu.rad.yaw + PI/2 - gimbal_angle_difference, g_imu.rad.yaw);
-// }
+    motor_data_odom.front_left = DJI_Motor_Get_Total_Angle(motors[0]) * physical_constants.R;
+    motor_data_odom.back_left = DJI_Motor_Get_Total_Angle(motors[1]) * physical_constants.R;
+    motor_data_odom.back_right = DJI_Motor_Get_Total_Angle(motors[2]) * physical_constants.R;
+    motor_data_odom.front_right = DJI_Motor_Get_Total_Angle(motors[3]) * physical_constants.R;
+}
+
+void Update_Omega() 
+{
+    switch ((int) g_board_comm_package.Ps) {
+        case 60:
+            omega = OMEGA_55W;
+            break;
+        case 65:
+            omega = OMEGA_65W;
+            break;
+        case 70:
+            omega = OMEGA_70W;
+            break;
+        case 75:
+            omega = OMEGA_75W;
+            break;
+        case 80:
+            omega = OMEGA_80W;
+            break;
+        case 85:
+            omega = OMEGA_85W;
+            break;
+        case 90:
+            omega = OMEGA_90W;
+            break;
+        default:
+            omega = OMEGA_55W;
+    }
+
+    if (g_robot_state.IS_SUPER_CAPACITOR_ENABLED) {
+        omega = 20.0f;
+        omega = Rescale_Omega();
+    }
+}
+
+float Rescale_Omega(void) {
+    float translation_speed = sqrtf(powf(g_remote.controller.left_stick.x / 660.0f * 4.0f, 2) + powf(g_remote.controller.left_stick.y / 660.0f * 4.0f, 2));
+    float spin_coeff = CHASSIS_RADIUS * omega / (translation_speed * 500.0f + CHASSIS_RADIUS * omega);
+    float target_omega = omega * spin_coeff;
+    return target_omega;
+}
--- Standard/src/launch_task.c
+++ Standard/src/launch_task.c
@@ -71,160 +71,145 @@ void Launch_Task_Init(){
     g_feed_motor = DJI_Motor_Init(&feed_speed_config,M2006);
 }
 
-void Launch_Ctrl_Loop(){
-
-    DJI_Motor_Set_Velocity(g_feed_motor, g_remote.controller.wheel/660.0f * 100.0f);    //TODO: Make launch right
-
-    if (g_remote.controller.left_switch == UP)
-    {
+void Launch_Ctrl_Loop()
+{
+    if(g_remote.controller.left_switch == UP){
         DJI_Motor_Set_Velocity(g_flywheel_left, -300);
         DJI_Motor_Set_Velocity(g_flywheel_right, -300);
 
+        DJI_Motor_Set_Velocity(g_feed_motor, 100);
     }
-    else
-    {
-        DJI_Motor_Set_Velocity(g_flywheel_left, 0);
-        DJI_Motor_Set_Velocity(g_flywheel_right, 0);
+    else{
+        DJI_Motor_Set_Velocity(g_flywheel_left, -00);
+        DJI_Motor_Set_Velocity(g_flywheel_right, -00);
+
+        DJI_Motor_Set_Velocity(g_feed_motor, 0);
+    }
+    // if (!g_robot_state.launch.IS_FIRING_ENABLED)
+    // {
+    //     stopFlywheel();
+    //     Laser_Off();
+    //     g_robot_state.launch.IS_FLYWHEEL_ENABLED = 0;
+    //     return;
+    // } else {
+    //     g_robot_state.launch.IS_FLYWHEEL_ENABLED = 1;
+    //     startFlywheel();
+    //     Laser_On();
+    // }
+
+    // if (g_robot_state.launch.IS_BUSY) { // check if we are in middle of a fire mode
+    //     switch (g_robot_state.launch.busy_mode)
+    //     {
+    //     case REJIGGLE:
+    //         rejiggle();
+    //         break;
+    //     case SINGLE_FIRE:
+    //         handleSingleFire();
+    //         break;
+    //     case BURST_FIRE:
+    //         break;
+    //     case FULL_AUTO:
+    //         handleFullAuto();
+    //         break;
+    //     default:
+    //         break;
+    //     }
+    // } else {
+    //     // Control loop for launch to see if new mode is set
+    //     switch (g_robot_state.launch.fire_mode)
+    //     {
+    //     case SINGLE_FIRE:
+    //         handleSingleFire();
+    //         break;
+    //     case BURST_FIRE:
+    //         // TODO: Complete 5 burst
+    //         break;
+    //     case FULL_AUTO:
+    //         handleFullAuto();
+    //         break;
+    //     default:
+    //         break;
+
+    //     }
+    // }
+}
+
+void resetRelPos() {
+    g_robot_state.launch.shooter_state.accum_angle = 0;
+    g_robot_state.launch.shooter_state.prev_time = xTaskGetTickCount();
+}
+
+#define ticksToDegrees(ticks) ((ticks) * 360.0f / DJI_MAX_TICKS)
+#define ticksToRad(ticks) ((ticks) * 360.0f / DJI_MAX_TICKS)
+#define degreesToTicks(degrees) ((degrees) * DJI_MAX_TICKS / 360.0f)
+float g_curr_angle = 0;
+// TODO check if at ref
+void handleSingleFire() {
+    if (g_robot_state.launch.IS_BUSY) {
+        if (DJI_Motor_Is_At_Angle(g_feed_motor, FEED_TOLERANCE)) // if shots fired :O then rejiggle
+        {
+            g_robot_state.launch.IS_BUSY = 0;
+            g_robot_state.launch.busy_mode = IDLE;
+            rejiggle();
+        }
+    }
+    else {
+        g_robot_state.launch.IS_BUSY = 1;
+        g_robot_state.launch.busy_mode = SINGLE_FIRE;
+        // set a new position reference x degrees forward
+        resetRelPos();
+
+        DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
+        g_curr_angle = DJI_Motor_Get_Total_Angle(g_feed_motor); // rad
+        DJI_Motor_Set_Angle(g_feed_motor, g_curr_angle + SHOT_ANGLE_OFFSET_RAD);
+        // DJI_Motor_Set_Velocity(g_feed_motor, FEED_RATE);
     }
 }
 
+void rejiggle() {
+    //set a position reference slightly back to prevent jams
+    float curr_angle_rad = DJI_Motor_Get_Total_Angle(g_feed_motor); // rad
 
-// void Launch_Task_Init()
-// {
+    if (g_robot_state.launch.IS_BUSY) { // if busy, means we already called so go back
+        if (DJI_Motor_Is_At_Angle(g_feed_motor, FEED_TOLERANCE))
+        {
+            g_robot_state.launch.busy_mode = IDLE;
+            g_robot_state.launch.IS_BUSY = 0;
+            DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
+            DJI_Motor_Set_Angle(g_feed_motor, curr_angle_rad + (SHOT_ANGLE_OFFSET_RAD / 2));
+        }
+    }
+    else {
+        g_robot_state.launch.busy_mode = REJIGGLE;
+        g_robot_state.launch.IS_BUSY = 1;
+        DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
+        DJI_Motor_Set_Angle(g_feed_motor, curr_angle_rad - (SHOT_ANGLE_OFFSET_RAD / 2));
+    }
+}
 
-//     Laser_Init();
-// }
+void handleFullAuto() {
+    if (g_robot_state.launch.IS_BUSY) {
+        if (g_robot_state.launch.fire_mode == NO_FIRE) {
+            DJI_Motor_Set_Control_Mode(g_feed_motor, VELOCITY_CONTROL);
+            DJI_Motor_Set_Velocity(g_feed_motor, 0);
+            g_robot_state.launch.IS_BUSY = 0;
+            g_robot_state.launch.busy_mode = IDLE;
+            rejiggle();
+        }
+    } else {
+        DJI_Motor_Set_Control_Mode(g_feed_motor, VELOCITY_CONTROL);
+        DJI_Motor_Set_Velocity(g_feed_motor, FEED_RATE);
+        g_robot_state.launch.IS_BUSY = 1;
+        g_robot_state.launch.busy_mode = FULL_AUTO;
+    }
+}
 
-// void Launch_Ctrl_Loop()
-// {
-//     // if (!g_robot_state.launch.IS_FIRING_ENABLED)
-//     // {
-//     //     stopFlywheel();
-//     //     Laser_Off();
-//     //     g_robot_state.launch.IS_FLYWHEEL_ENABLED = 0;
-//     //     return;
-//     // } else {
-//     //     g_robot_state.launch.IS_FLYWHEEL_ENABLED = 1;
-//     //     startFlywheel();
-//     //     Laser_On();
-//     // }
+void startFlywheel() {
+    DJI_Motor_Set_Velocity(g_flywheel_left, -320);
+    DJI_Motor_Set_Velocity(g_flywheel_right, -320);
+}
 
-//     // if (g_robot_state.launch.IS_BUSY) { // check if we are in middle of a fire mode
-//     //     switch (g_robot_state.launch.busy_mode)
-//     //     {
-//     //     case REJIGGLE:
-//     //         rejiggle();
-//     //         break;
-//     //     case SINGLE_FIRE:
-//     //         handleSingleFire();
-//     //         break;
-//     //     case BURST_FIRE:
-//     //         break;
-//     //     case FULL_AUTO:
-//     //         handleFullAuto();
-//     //         break;
-//     //     default:
-//     //         break;
-//     //     }
-//     // } else {
-//     //     // Control loop for launch to see if new mode is set
-//     //     switch (g_robot_state.launch.fire_mode)
-//     //     {
-//     //     case SINGLE_FIRE:
-//     //         handleSingleFire();
-//     //         break;
-//     //     case BURST_FIRE:
-//     //         // TODO: Complete 5 burst
-//     //         break;
-//     //     case FULL_AUTO:
-//     //         handleFullAuto();
-//     //         break;
-//     //     default:
-//     //         break;
-
-//     //     }
-//     // }
-
-//     
-
-// }
-
-// void resetRelPos() {
-//     g_robot_state.launch.shooter_state.accum_angle = 0;
-//     g_robot_state.launch.shooter_state.prev_time = xTaskGetTickCount();
-// }
-
-// #define ticksToDegrees(ticks) ((ticks) * 360.0f / DJI_MAX_TICKS)
-// #define ticksToRad(ticks) ((ticks) * 360.0f / DJI_MAX_TICKS)
-// #define degreesToTicks(degrees) ((degrees) * DJI_MAX_TICKS / 360.0f)
-// float g_curr_angle = 0;
-// // TODO check if at ref
-// void handleSingleFire() {
-//     if (g_robot_state.launch.IS_BUSY) {
-//         if (DJI_Motor_Is_At_Angle(g_feed_motor, FEED_TOLERANCE)) // if shots fired :O then rejiggle
-//         {
-//             g_robot_state.launch.IS_BUSY = 0;
-//             g_robot_state.launch.busy_mode = IDLE;
-//             rejiggle();
-//         }
-//     }
-//     else {
-//         g_robot_state.launch.IS_BUSY = 1;
-//         g_robot_state.launch.busy_mode = SINGLE_FIRE;
-//         // set a new position reference x degrees forward
-//         resetRelPos();
-
-//         DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
-//         g_curr_angle = DJI_Motor_Get_Total_Angle(g_feed_motor); // rad
-//         DJI_Motor_Set_Angle(g_feed_motor, g_curr_angle + SHOT_ANGLE_OFFSET_RAD);
-//         // DJI_Motor_Set_Velocity(g_feed_motor, FEED_RATE);
-//     }
-// }
-
-// void rejiggle() {
-//     //set a position reference slightly back to prevent jams
-//     float curr_angle_rad = DJI_Motor_Get_Total_Angle(g_feed_motor); // rad
-
-//     if (g_robot_state.launch.IS_BUSY) { // if busy, means we already called so go back
-//         if (DJI_Motor_Is_At_Angle(g_feed_motor, FEED_TOLERANCE))
-//         {
-//             g_robot_state.launch.busy_mode = IDLE;
-//             g_robot_state.launch.IS_BUSY = 0;
-//             DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
-//             DJI_Motor_Set_Angle(g_feed_motor, curr_angle_rad + (SHOT_ANGLE_OFFSET_RAD / 2));
-//         }
-//     }
-//     else {
-//         g_robot_state.launch.busy_mode = REJIGGLE;
-//         g_robot_state.launch.IS_BUSY = 1;
-//         DJI_Motor_Set_Control_Mode(g_feed_motor, POSITION_CONTROL_TOTAL_ANGLE);
-//         DJI_Motor_Set_Angle(g_feed_motor, curr_angle_rad - (SHOT_ANGLE_OFFSET_RAD / 2));
-//     }
-// }
-
-// void handleFullAuto() {
-//     if (g_robot_state.launch.IS_BUSY) {
-//         if (g_robot_state.launch.fire_mode == NO_FIRE) {
-//             DJI_Motor_Set_Control_Mode(g_feed_motor, VELOCITY_CONTROL);
-//             DJI_Motor_Set_Velocity(g_feed_motor, 0);
-//             g_robot_state.launch.IS_BUSY = 0;
-//             g_robot_state.launch.busy_mode = IDLE;
-//             rejiggle();
-//         }
-//     } else {
-//         DJI_Motor_Set_Control_Mode(g_feed_motor, VELOCITY_CONTROL);
-//         DJI_Motor_Set_Velocity(g_feed_motor, FEED_RATE);
-//         g_robot_state.launch.IS_BUSY = 1;
-//         g_robot_state.launch.busy_mode = FULL_AUTO;
-//     }
-// }
-
-// void startFlywheel() {
-//     DJI_Motor_Set_Velocity(g_flywheel_left, -320);
-//     DJI_Motor_Set_Velocity(g_flywheel_right, -320);
-// }
-
-// void stopFlywheel() {
-//     DJI_Motor_Set_Velocity(g_flywheel_left, 0);
-//     DJI_Motor_Set_Velocity(g_flywheel_right, 0);
-// }
+void stopFlywheel() {
+    DJI_Motor_Set_Velocity(g_flywheel_left, 0);
+    DJI_Motor_Set_Velocity(g_flywheel_right, 0);
+}
