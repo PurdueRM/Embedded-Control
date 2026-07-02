9a447088add5744094aed303ce930db6f3faca25
Yi <0418yicheng@gmail.com>
2026-07-02 08:46:58 -0400

    Keyboard controls work, working on spintop and launch

--- Standard/src/gimbal_task.c
+++ Standard/src/gimbal_task.c
@@ -28,7 +28,7 @@ void Gimbal_Task_Init(){
     DM_Motor_Config_t yaw_motor_config = {
         .can_bus = 1,
         .control_mode = DM_MOTOR_MIT,
-        .pos_offset = -1.616,
+        .pos_offset = -3.2866,
         .rx_id = 0x11,
         .tx_id = 0x01,
         .disable_behavior = DM_MOTOR_ZERO_CURRENT,
@@ -53,11 +53,10 @@ void Gimbal_Task_Init(){
 
 void Gimbal_Ctrl_Loop(){
     // Control loop for gimbal
-    if(fabs(g_remote.controller.right_stick.x) > 5.0f)
-        g_robot_state.gimbal.yaw_angle -= g_remote.controller.right_stick.x/660.0f * 0.01f;
+    g_robot_state.gimbal.yaw_angle -= g_remote.controller.right_stick.x/660.0f * 0.01f +  g_remote.mouse.x / 100000.0f;
 
     //Gimbal follow imu PID
-    tmp_yaw_angle_diff = g_robot_state.gimbal.yaw_angle - g_imu.rad.yaw - PI/9\'';
+    tmp_yaw_angle_diff = g_robot_state.gimbal.yaw_angle - (g_imu.rad.yaw - 0.09f);
     __MAP_ANGLE_TO_UNIT_CIRCLE(tmp_yaw_angle_diff);
     g_yaw_torque = PID(&gimbal_imu_pid, tmp_yaw_angle_diff);
     __MAX_LIMIT(g_yaw_torque, -1.5f, 1.5f)
@@ -65,7 +64,7 @@ void Gimbal_Ctrl_Loop(){
     DM_Motor_Enable_Motor(g_yaw);
     DM_Motor_Ctrl_MIT_PD(g_yaw, 0.0f, 0.0f, g_yaw_torque, 0.0f, 0.0f);
 
-    g_robot_state.gimbal.pitch_angle -= g_remote.controller.right_stick.y / 660.0f * 0.01f;
+    g_robot_state.gimbal.pitch_angle -= g_remote.controller.right_stick.y / 660.0f * 0.01f + g_remote.mouse.y/100000.0f;
     __MAX_LIMIT(g_robot_state.gimbal.pitch_angle, -0.45f, 0.4f);
     
     DM_Motor_Enable_Motor(g_pitch);
